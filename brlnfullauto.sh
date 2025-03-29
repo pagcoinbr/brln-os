9d2a293c15f9f310ae941d94bf334b43460a76ba
pagcoinbr <167659900+pagcoinbr@users.noreply.github.com>
2025-03-29 14:07:17 -0300

    Update brlnfullauto.sh

--- brlnfullauto.sh
+++ brlnfullauto.sh
@@ -10,15 +10,6 @@ LN_DDIR=/data/lnd
 LNDG_DIR=/home/admin/lndg
 VERSION_THUB=0.13.31
 
-# Cores
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[1;34m'
-MAGENTA='\033[1;35m'
-CYAN='\033[1;36m'
-NC='\033[0m' # Sem cor
-
 update_and_upgrade() {
 sudo apt update && sudo apt full-upgrade -y
 }
@@ -882,5 +873,13 @@ menu() {
   esac
 }
 
+# Cores
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+MAGENTA='\033[1;35m'
+CYAN='\033[1;36m'
+NC='\033[0m' # Sem cor
 
 menu
