8217e06ae487cff7100c605dd3464d738a1aac17
pagcoinbr <lfmolinadc@gmail.com>
2025-04-02 23:13:10 -0300

    1

--- html/adm_scripts/update_apt.sh
+++ html/adm_scripts/update_apt.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-# Atualiza os pacotes do sistema e recarrega o serviço Tor  
-  sudo apt update && sudo apt upgrade -y
-  sudo systemctl reload tor
-  echo "Os pacotes do sistema foram atualizados!"
+# #!/bin/bash
+# # Atualiza os pacotes do sistema e recarrega o serviço Tor  
+  sudo systemctl restart tor
+  echo "O tor foi reiniciado com sucesso!"
