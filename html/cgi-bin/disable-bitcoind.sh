70249a8e81411ca0d383c48f1a2b1ccbc9c6a193
pagcoinbr <lfmolinadc@gmail.com>
2025-04-05 15:32:52 -0300

    update

--- html/cgi-bin/disable-bitcoind.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Content-type: application/json"
-echo ""
-
-if sudo systemctl stop bitcoind && sudo systemctl disable bitcoind; then
-    echo '{"status":"ok", "mensagem":"Bitcoind foi DESLIGADO com sucesso"}'
-else
-    echo '{"status":"erro", "mensagem":"Erro ao desligar o Bitcoind"}'
-fi
