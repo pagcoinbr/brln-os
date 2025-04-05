de87109b01f4968d9fc0021e85c5990c238a7a70
pagcoinbr <lfmolinadc@gmail.com>
2025-04-05 10:27:16 -0300

    update toogle

--- html/cgi-bin/toogle_bitcoind.sh
+++ html/cgi-bin/toogle_bitcoind.sh
@@ -2,7 +2,13 @@
 echo "Content-type: application/json"
 echo ""
 
-read -r input
+read_input() {
+  input=""
+  while IFS= read -r line; do
+    input="$input$line"
+  done
+}
+read_input
 
 if echo "$input" | grep -q '"acao":"ligar-bitcoind"'; then
     if sudo systemctl start bitcoind && sudo systemctl enable bitcoind; then
