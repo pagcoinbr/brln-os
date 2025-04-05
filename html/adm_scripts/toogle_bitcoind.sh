ece7bd1bb3bdc2e71c2c17dde775545d12bcb917
pagcoinbr <lfmolinadc@gmail.com>
2025-04-05 00:20:19 -0300

    toogle ok

--- html/adm_scripts/toogle_bitcoind.sh
+++ html/adm_scripts/toogle_bitcoind.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+echo "Content-type: application/json"
+echo ""
 
-disable_bitcoind() {
-    sudo systemctl stop bitcoind
-    sudo systemctl disable bitcoind
-}
+read -r input
 
-if disable_bitcoind; then
-    echo '{"status":"ok", "mensagem":"Bitcoind foi DESLIGADO com sucesso"}'
+if echo "$input" | grep -q '"acao":"ligar-bitcoind"'; then
+    if sudo systemctl start bitcoind && sudo systemctl enable bitcoind; then
+        echo '{"status":"ok", "mensagem":"Bitcoind foi LIGADO com sucesso"}'
+    else
+        echo '{"status":"erro", "mensagem":"Erro ao ligar o Bitcoind"}'
+    fi
+
+elif echo "$input" | grep -q '"acao":"desligar-bitcoind"'; then
+    if sudo systemctl stop bitcoind && sudo systemctl disable bitcoind; then
+        echo '{"status":"ok", "mensagem":"Bitcoind foi DESLIGADO com sucesso"}'
+    else
+        echo '{"status":"erro", "mensagem":"Erro ao desligar o Bitcoind"}'
+    fi
+
 else
-    echo '{"status":"erro", "mensagem":"Falha ao desligar o Bitcoind"}'
+    echo '{"status":"erro", "mensagem":"Ação inválida"}'
 fi
