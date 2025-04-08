3fd303715a5fce6cd7c1447ee0946ae280805109
pagcoinbr <lfmolinadc@gmail.com>
2025-04-08 18:27:50 -0300

    Update brlnfullauto.sh

--- brlnfullauto.sh
+++ brlnfullauto.sh
@@ -132,8 +132,8 @@ download_lnd() {
     echo "####################################################################################### WARNING: GPG SIGNATURE NOT VERIFIED.##################################################################################################################################################"
     exit 1
   fi
-  tar -xzf tar -xvf lnd-linux-$arch_lnd-v$VERSION-beta.tar.gz
-  sudo install -m 0755 -o root -g root -t /usr/local/bin lnd-linux-$arch_lnd-v$VERSION-beta/lnd lnd-linux-$arch_lnd-v$VERSION-beta/lncli lnd-linux-$arch_lnd-v$LND_VERSION-beta/lncli
+  tar -xzf tar -xvf lnd-linux-$arch_lnd-v$LND_VERSION-beta.tar.gz
+  sudo install -m 0755 -o root -g root -t /usr/local/bin lnd-linux-$arch_lnd-v$LND_VERSION-beta/lnd lnd-linux-$arch_lnd-v$LND_VERSION-beta/lncli lnd-linux-$arch_lnd-v$LND_VERSION-beta/lncli
   sudo rm -r lnd-linux-$arch_lnd-v$LND_VERSION-beta lnd-linux-$arch_lnd-v$LND_VERSION-beta.tar.gz manifest-roasbeef-v$LND_VERSION-beta.sig manifest-roasbeef-v$LND_VERSION-beta.sig.ots manifest-v$LND_VERSION-beta.txt manifest-v$LND_VERSION-beta.txt.ots
 }
 
