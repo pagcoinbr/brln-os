5fde9223475f754f29194c674fb60003cf403817
pagcoinbr <lfmolinadc@gmail.com>
2025-04-08 01:25:34 -0300

    Update brlnfullauto.sh

--- brlnfullauto.sh
+++ brlnfullauto.sh
@@ -973,8 +973,7 @@ simple_lnwallet () {
   sudo systemctl daemon-reload
   sudo systemctl enable simple-lnwallet
   sudo systemctl start simple-lnwallet
-  # Extrair a porta do comando systemctl status e exibir para o usuário
-  sudo systemctl status simple-lnwallet.service | grep -oP 'porta :\K[0-9]+')
+  sudo systemctl status simple-lnwallet.service
   echo -e "${YELLOW} Acesse o endereço de IP do seu nó:${NC}"
   echo -e "${YELLOW} http://<IP_DO_TAILSCALE>:<PORTA>${NC}"
 }
