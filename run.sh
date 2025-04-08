740fdaa79b54891b06c282f7ccb8daebd0e3c96f
pagcoinbr <lfmolinadc@gmail.com>
2025-04-08 13:13:02 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,12 +18,12 @@ if [[ -d "$INSTALL_DIR" ]]; then
   echo -e "${GREEN}Diretório brlnfullauto já presente.${NC}"
 else
   echo -e "${RED}Diretório brlnfullauto não encontrado, baixando...${NC}"
-  git clone https://github.com/REDACTED_USERbr/brlnfullauto.git "$INSTALL_DIR" >> "$INSTALL_DIR/install.log" 2>&1
+  git clone https://github.com/REDACTED_USERbr/brlnfullauto.git "$INSTALL_DIR" >> /dev/null 2>&1
   sleep 2
   cd "$INSTALL_DIR"
   touch install.sh
-  git stash >> install.log 2>&1
-  git checkout v0.8-beta >> install.log 2>&1
+  git stash /dev/null 2>&1
+  git checkout v0.8-beta >> /dev/null 2>&1
 fi
 
 chmod +x "$INSTALL_DIR/brlnfullauto.sh"
