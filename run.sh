5b1f5f2eee26ef1533bed554b4931a0d2da30736
pagcoinbr <lfmolinadc@gmail.com>
2025-04-08 18:18:20 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,6 +20,8 @@ brln_check () {
     sudo chown -R admin:admin "$INSTALL_DIR"
     sleep 2
     sudo -u admin git -C "$INSTALL_DIR" checkout main
+    git stash
+    git pull
   fi
 
   sudo usermod -aG sudo,adm,cdrom,dip,plugdev,lxd admin
