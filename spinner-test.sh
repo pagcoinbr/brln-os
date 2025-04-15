6d86dc6963cba6f1a8a8e295975b80471425bfbc
Redinpais <redinpais@yahoo.com>
2025-04-15 22:58:12 +0100

    Create spinner-test.sh

---
+++ spinner-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Função de spinner
+spinner() {
+    local pid=$!
+    local delay=0.1
+    local spinstr='|/-\'
+    echo -n " "
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+# Exemplo de comando demorado simulado
+echo -n "Simulando tarefa demorada..."
+(sleep 5) & spinner
+echo "✔ Feito!"
+
