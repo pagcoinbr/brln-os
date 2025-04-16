51d325c0f1f1c918cd0668f3cc7dbc81c52a316c
Redinpais <redinpais@yahoo.com>
2025-04-16 02:14:53 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ brln_check () {
     echo -e "${YELLOW}Digite a senha do usuário admin para continuar...${NC}"
   else
     echo -e "${RED}Diretório brlnfullauto não encontrado, baixando como admin...${NC}"
-    sudo -u admin git clone https://github.com/Redinpais/brlnfullauto.git "$INSTALL_DIR"
+    sudo -u admin git clone https://github.com/pagcoinbr/brlnfullauto.git "$INSTALL_DIR"
     sudo chown -R admin:admin "$INSTALL_DIR"
     sleep 2
     sudo -u admin git -C "$INSTALL_DIR" checkout main
