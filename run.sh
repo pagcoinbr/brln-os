d291b5081ac31843c733671657efe3ebabe1748a
pagcoinbr <lfmolinadc@gmail.com>
2025-04-18 21:44:46 -0300

    Adição de Terminal Web

--- run.sh
+++ run.sh
@@ -100,4 +100,23 @@ elif [[ $create_user == "no" ]]; then
 fi
 }
 
+terminal_web () {
+  if [[ ! -f /usr/local/bin/gotty ]]; then
+    echo -e "${GREEN} Instalando Terminal Web... ${NC}"
+    wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz
+    tar -xvzf gotty_linux_amd64.tar.gz
+    sudo mv gotty /usr/local/bin
+    sudo cp /home/brlnfullauto/services/gotty.service /etc/systemd/system/gotty.service
+    sudo systemctl enable gotty.service
+    sudo systemctl start gotty.service
+    echo -e "${GREEN} gotty instalado com sucesso! ${NC}"
+    echo -e "${GREEN} Acesse o terminal web em: http://$(hostname -I | awk '{print $1}') ${NC}"
+    exit 0
+  else
+    echo -e "${GREEN} gotty já está instalado! ${NC}"
+    main_call
+  fi
+}
+
 main_call
+terminal_web
