5f3ca647ead658bbb46e83919f33e42f5c5a475b
pagcoinbr <lfmolinadc@gmail.com>
2025-04-19 15:13:11 -0300

    Update brlnfullauto.sh

--- brlnfullauto.sh
+++ brlnfullauto.sh
@@ -120,8 +120,9 @@ terminal_web() {
   if [[ ! -f /usr/local/bin/gotty ]]; then
     echo -e "${GREEN} Instalando Terminal Web... ${NC}"
     # Baixa o binário como admin
-    sudo -u admin wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz -O /home/admin/gotty_linux_amd64.tar.gz
+    sudo -v
     update_and_upgrade >> /dev/null 2>&1 & spinner
+    sudo -u admin wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz -O /home/admin/gotty_linux_amd64.tar.gz
     # Extrai como admin
     sudo -u admin tar -xvzf /home/admin/gotty_linux_amd64.tar.gz -C /home/admin/
     # Move binário para /usr/local/bin
