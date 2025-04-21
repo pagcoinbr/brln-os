941329a830128cb71ae6b3490eaec77c38d67686
pagcoinbr <lfmolinadc@gmail.com>
2025-04-21 02:20:33 -0300

    Update brlnfullauto.sh

--- brlnfullauto.sh
+++ brlnfullauto.sh
@@ -23,44 +23,6 @@ MAGENTA='\033[1;35m'
 CYAN='\033[1;36m'
 NC='\033[0m' # Sem cor
 
-spinner() {
-    local pid=$!
-    local delay=0.2
-    local max=${SPINNER_MAX:-20}
-    local count=0
-    local spinstr='|/-\\'
-    local j=0
-
-    tput civis
-
-    # Monitorar processo
-    while kill -0 "$pid" 2>/dev/null; do
-        local emoji=""
-        for ((i=0; i<=count; i++)); do
-            emoji+="⚡"
-        done
-
-        local spin_char="${spinstr:j:1}"
-        j=$(( (j + 1) % 4 ))
-        count=$(( (count + 1) % (max + 1) ))
-
-        printf "\r\033[KInstalando $app no seu BRLN bolt...${YELLOW}%s${NC} ${CYAN}[%s]${NC}" "$emoji" "$spin_char"
-        sleep "$delay"
-    done
-
-    wait "$pid"
-    exit_code=$?
-
-    tput cnorm
-    if [[ $exit_code -eq 0 ]]; then
-        printf "\r\033[K${GREEN}✔️ Processo finalizado com sucesso!${NC}\n"
-    else
-        printf "\r\033[K${RED}❌ Processo finalizado com erro (código: $exit_code)${NC}\n"
-    fi
-
-    return $exit_code
-}
-
 update_and_upgrade() {
 echo "Instalando Apache..."
 sudo -v
@@ -1144,6 +1106,44 @@ system_preparations () {
   echo -e "${YELLOW}Na pior das hipóteses, até 30 minutos...${NC}"
   install_tor
   install_nodejs
+}
+
+spinner() {
+    local pid=$!
+    local delay=0.2
+    local max=${SPINNER_MAX:-20}
+    local count=0
+    local spinstr='|/-\\'
+    local j=0
+
+    tput civis
+
+    # Monitorar processo
+    while kill -0 "$pid" 2>/dev/null; do
+        local emoji=""
+        for ((i=0; i<=count; i++)); do
+            emoji+="⚡"
+        done
+
+        local spin_char="${spinstr:j:1}"
+        j=$(( (j + 1) % 4 ))
+        count=$(( (count + 1) % (max + 1) ))
+
+        printf "\r\033[KInstalando $app no seu BRLN bolt...${YELLOW}%s${NC} ${CYAN}[%s]${NC}" "$emoji" "$spin_char"
+        sleep "$delay"
+    done
+
+    wait "$pid"
+    exit_code=$?
+
+    tput cnorm
+    if [[ $exit_code -eq 0 ]]; then
+        printf "\r\033[K${GREEN}✔️ Processo finalizado com sucesso!${NC}\n"
+    else
+        printf "\r\033[K${RED}❌ Processo finalizado com erro (código: $exit_code)${NC}\n"
+    fi
+
+    return $exit_code
 }
 
 menu() {
