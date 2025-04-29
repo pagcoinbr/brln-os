38909b1861560cb1629bab38d793fda3f62160a3
pagcoinbr <lfmolinadc@gmail.com>
2025-04-29 19:37:56 -0300

    Update brunel.sh

--- brunel.sh
+++ brunel.sh
@@ -1016,7 +1016,7 @@ get_simple_wallet () {
   fi
   cp /home/admin/brlnfullauto/local_apps/simple-lnwallet/$simple_arch /home/admin
   if [[ ! -f /home/admin/simple-lnwallet-rpi ]]; then
-  mv /home/admin/$simple_arch /home/admin/simple-lnwallet
+  mv /home/admin/simple-lnwallet-rpi /home/admin/simple-lnwallet
   fi
   chmod +x /home/admin/simple-lnwallet
   sudo apt install xxd -y
