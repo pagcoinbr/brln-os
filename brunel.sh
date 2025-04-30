4b66f795cc9001e3ac9dfa3dd593aaafc02bcbde
pagcoinbr <lfmolinadc@gmail.com>
2025-04-30 20:31:35 -0300

    radio update

--- brunel.sh
+++ brunel.sh
@@ -203,6 +203,7 @@ terminal_web() {
   if [[ ! -f /usr/local/bin/gotty ]]; then
     # Baixa o binário como admin
     update_and_upgrade
+    radio_update
     gotty_install
     tailscale_vpn
     opening
@@ -1218,6 +1219,26 @@ submenu_opcoes() {
       submenu_opcoes
       ;;
   esac
+}
+
+radio_update () {
+  # Caminho do script que deve rodar a cada hora
+  SCRIPT="/home/admin/brlnfullauto/html/radio/radioupdate_radio.sh"
+
+  # Linha que será adicionada ao crontab
+  CRON_LINE="0 * * * * $SCRIPT >> /var/log/update_radio.log 2>&1"
+
+  # Verifica se já existe no crontab
+  crontab -l 2>/dev/null | grep -F "$SCRIPT" > /dev/null
+
+  if [ $? -eq 0 ]; then
+    echo "✅ A entrada do crontab já existe. Nenhuma alteração feita."
+  else
+    echo "➕ Adicionando entrada ao crontab..."
+    (crontab -l 2>/dev/null; echo "$CRON_LINE") | crontab -
+    echo "✅ Entrada adicionada com sucesso!"
+  fi
+  sudo chmod +x $SCRIPT
 }
 
 ip_finder () {
