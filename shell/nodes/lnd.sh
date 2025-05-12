66e61cf18177c513632fe02724d5007fdbd6cd31
pagcoinbr <lfmolinadc@gmail.com>
2025-05-11 22:19:22 -0300

    Update lnd.sh

--- shell/nodes/lnd.sh
+++ shell/nodes/lnd.sh
@@ -60,7 +60,7 @@ download_lnd() {
   else
     arch_lnd="arm64"
   fi
-  tar -xzf lnd-linux-$arch_lnd-v$LND_VERSION-beta.tar.gz -C "$LOCAL_APPS/lnd" 
+  tar -xvzf "$LOCAL_APPS/lnd/lnd-linux-$arch_lnd-v$LND_VERSION-beta.tar.gz" -C "$LOCAL_APPS/lnd" 
   sudo cp "$LOCAL_APPS/lnd/lnd-linux-$arch_lnd-v$LND_VERSION-beta/lnd /usr/local/bin"
   sudo cp "$LOCAL_APPS/lnd/lnd-linux-$arch_lnd-v$LND_VERSION-beta/lncli /usr/local/bin"
 }
