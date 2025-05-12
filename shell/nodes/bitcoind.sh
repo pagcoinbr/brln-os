ea86cfda71eea86253e1c504cd1d0b7880d6e94b
pagcoinbr <lfmolinadc@gmail.com>
2025-05-11 21:40:55 -0300

    fine tuning

--- shell/nodes/bitcoind.sh
+++ shell/nodes/bitcoind.sh
@@ -3,24 +3,13 @@ source ~/brlnfullauto/shell/.env
 
 install_bitcoind() {
   local file_path="/home/admin/brlnfullauto/conf_files/bitcoin.conf"
-  set -e
   if [[ $arch == "x86_64" ]]; then
     arch_btc="x86_64"
   else
     arch_btc="aarch64"
   fi
-
-  cd /tmp
-  wget https://bitcoincore.org/bin/bitcoin-core-$BTC_VERSION/bitcoin-$BTC_VERSION-$arch_btc-linux-gnu.tar.gz
-  wget https://bitcoincore.org/bin/bitcoin-core-$BTC_VERSION/SHA256SUMS
-  wget https://bitcoincore.org/bin/bitcoin-core-$BTC_VERSION/SHA256SUMS.asc
-  sha256sum --ignore-missing --check SHA256SUMS
-  # Importação das chaves PGP oficiais do Bitcoin Core
-  curl https://bitcoincore.org/keys/keys.txt | gpg --import
-  # Verificação da assinatura PGP
-  gpg --verify SHA256SUMS.asc
-  tar -xzvf bitcoin-$BTC_VERSION-$arch_btc-linux-gnu.tar.gz
-  sudo install -m 0755 -o root -g root -t /usr/local/bin bitcoin-$BTC_VERSION/bin/bitcoin-cli bitcoin-$BTC_VERSION/bin/bitcoind
+  tar -xzvf ~/brlnfullauto/local_apps/bitcoind/bitcoin-$BTC_VERSION-$arch_btc-linux-gnu.tar.gz
+  sudo install -m 0755 -o root -g root -t /usr/local/bin ~/brlnfullauto/local_apps/bitcoind/bitcoin-$BTC_VERSION/bin/bitcoin-cli bitcoin-$BTC_VERSION/bin/bitcoind
   sudo mkdir -p /data/bitcoin
   sudo chown admin:admin /data/bitcoin
   ln -s /data/bitcoin /home/admin/.bitcoin
@@ -28,13 +17,18 @@ install_bitcoind() {
   sudo chown admin:admin /data/bitcoin/bitcoin.conf
   sudo chmod 640 /data/bitcoin/bitcoin.conf
   cd /home/admin/.bitcoin
-  wget https://raw.githubusercontent.com/bitcoin/bitcoin/master/share/rpcauth/rpcauth.py
+  sudo cp ~/brlnfullauto/local_apps/bitcoind/rpcauth.py /home/admin/.bitcoin/rpcauth.py
   sudo sed -i "54s|.*|$(python3 rpcauth.py minibolt $rpcpsswd > /home/admin/.bitcoin/rpc.auth | grep '^rpcauth=')|" /home/admin/brlnfullauto/conf_files/bitcoin.conf
   sudo cp $SERVICES_DIR/bitcoind.service /etc/systemd/system/bitcoind.service
   sudo systemctl enable bitcoind
   sudo systemctl start bitcoind
   sudo ss -tulpn | grep bitcoind
-  echo "Bitcoind instalado com sucesso!"
+  if[[ $? -ne 0 ]]; then
+    echo "Erro ao iniciar o serviço bitcoind. Verifique os logs para mais detalhes."
+    exit 1
+  else
+    echo "Bitcoind instalado com sucesso!"
+  fi
 }
 
 spinner() {
