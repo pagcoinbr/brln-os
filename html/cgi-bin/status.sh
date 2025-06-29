af6ea66a1e786e8ee6cdd2562cd72f1129aefc73
naghust <naghust@proton.me>
2025-06-29 06:04:01 -0300

    Add buttons and information for PeerSwap and Elements (#40)
    Obrigado pela contribuição colega, junto faremos o bitcoin mais forte.

--- html/cgi-bin/status.sh
+++ html/cgi-bin/status.sh
@@ -13,6 +13,7 @@ echo "CPU: $cpu_usage"
 echo "RAM: $ram_usage"
 echo "LND: $(check_service lnd)"
 echo "Bitcoind: $(check_service bitcoind)"
+echo "Elementsd: $(check_service elementsd)"
 echo "Tor: $(check_service tor)"
 
 # Verifica se bitcoind.rpchost está ativo ou comentado
