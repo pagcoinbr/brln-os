9203be328bcf109f32531143d076edc7351f4927
pagcoin <lfmolinadc@gmail.com>
2025-11-20 05:10:05 +0000

    Remove large binaries and archives from tracking, update .gitignore
    - Removed container/ directory with large Docker images and binaries (~150MB+)
    - Removed simple-lnwallet binaries (~50MB)
    - Removed gotty archives (~6MB)
    - Updated .gitignore to prevent future commits of binary files
    - These files should be downloaded fresh during deployment

--- container/apps/electrum/electrum.sh
+++
@@ -1,4 +0,0 @@
-install_electrum () {
-    sudo apt install cargo clang cmake
-    mkdir /home/admin/rust
-}
--- container/nodes/tor/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ensure correct permissions for tor directory
-chmod 750 /var/lib/tor
-chown debian-tor:tor-access /var/lib/tor
-
-# Switch to debian-tor user and start tor with the provided arguments
-exec runuser -u debian-tor -- "$@"
